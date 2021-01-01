244e12a51533b865d3f3c3831af735509e425f26
Muhammad Kumail <mkumailofficial@gmail.com>
2021-01-01 11:06:22 +0500

    dependencies

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-var express = require('express'),
+var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
-  mongoose = require('mongoose'),
-  Campgrounds = require('./models/campground'),
+  mongoose = require("mongoose"),
+  Campgrounds = require("./models/campground"),
   seedDB = require("./seeds"),
-  Comment = require('./models/comment'),
-  passport = require('passport'),
-  LocalStrategy = require('passport-local'),
-  User = require('./models/user'),
+  Comment = require("./models/comment"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  User = require("./models/user"),
   port = process.env.PORT || 8080;
 
-var indexRoute = require('./routes/index'),
-  campgroundsRoute = require('./routes/campgrounds'),
-  commentRoute = require('./routes/comments');
+var indexRoute = require("./routes/index"),
+  campgroundsRoute = require("./routes/campgrounds"),
+  commentRoute = require("./routes/comments");
 
-mongoose.connect('mongodb://localhost/yelp_camp_v3', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost/yelp_camp_v3", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //passporrt configuration
-app.use(require('express-session')({
-  secret: "Once again rusty wins cutest dogs!",
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    secret: "Once again rusty wins cutest dogs!",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -42,11 +47,12 @@ app.use(function (req, res, next) {
 app.use("/campgrounds", campgroundsRoute);
 app.use("/campgrounds/:id/comments", commentRoute);
 app.use("/", indexRoute);
+app.use("*", (req, res) => {
+  res.send("Opps page not found");
+});
 
 //seedDB();
 
 app.listen(port, function () {
   console.log("\nServer started successfully: port '" + port + "'");
 });
-
-
